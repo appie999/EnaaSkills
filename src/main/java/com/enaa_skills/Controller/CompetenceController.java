7b745a4ca5107a7f8d941eb7f58a3b371da37945
appie999 <aitoutmghartfarid@gmail.com>
2025-07-01 15:05:50 +0100

    finish competence crud

---
+++ src/main/java/com/enaa_skills/Controller/CompetenceController.java
@@ -0,0 +1,42 @@
+package com.enaa_skills.Controller;
+
+import com.enaa_skills.Dto.CompetenceDto;
+import com.enaa_skills.Service.CompetanceService;
+import jdk.dynalink.linker.LinkerServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/competence")
+public class CompetenceController {
+
+    @Autowired
+    private CompetanceService competanceService;
+
+    @PostMapping("/")
+    public CompetenceDto saveCompetence(@RequestBody CompetenceDto competenceDto){
+        return competanceService.saveCompetance(competenceDto);
+    }
+
+    @GetMapping
+    public List<CompetenceDto> getAllCompetences(){
+        return competanceService.getAllCompetances();
+    }
+
+    @GetMapping("/{id}")
+    public CompetenceDto getCompetenceById(@PathVariable Long id){
+        return competanceService.getCompetanceById(id);
+    }
+
+    @PutMapping("/{id}")
+    public CompetenceDto editCompetence(@PathVariable Long id, @RequestBody CompetenceDto competenceDto){
+        return competanceService.editCompetance(id, competenceDto);
+    }
+
+    @DeleteMapping("/")
+    public void deleteCompetance(Long id){
+        competanceService.deleteCompetance(id);
+    }
+}
--- src/main/java/com/enaa_skills/Mapper/SousCompetanceMapper.java
+++ src/main/java/com/enaa_skills/Mapper/SousCompetanceMapper.java
@@ -4,6 +4,7 @@ import com.enaa_skills.Dto.SousCompetenceDto;
 import com.enaa_skills.Entity.SousCompetence;
 import org.mapstruct.Mapper;
 
+
 @Mapper(componentModel = "spring")
 public interface SousCompetanceMapper {
 
